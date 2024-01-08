08d48c43033480b9e4287b09d5fa9f6050458f42
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2024-01-08 11:36:22 -0800

    Change the "select" to radio button

--- components/form-fields/form-field.tsx
+++ components/form-fields/form-field.tsx
@@ -1,14 +1,15 @@
 import { RegularNode } from '@/lib/parser/node';
-import { useState } from 'react';
+import { use, useId, useState } from 'react';
+import { RadioGroup, RadioGroupItem } from '../ui/radio-group';
+import { Label } from '../ui/label';
 
 export default function FormField({ node }: { node: RegularNode }) {
-  const [value, setValue] = useState<string | number>(
-    node.value || node.marker.defaultValue || ''
-  );
+  const [value, setValue] = useState<string | number>(node.value || node.marker.defaultValue || '');
   const setNodeValue = (value: string) => {
     node.value = value;
     setValue(value);
   };
+  const id = useId();
 
   switch (node.type) {
     case 'text':
@@ -66,17 +67,14 @@ export default function FormField({ node }: { node: RegularNode }) {
           <div className="label">
             <span className="label-text text-lg">{node.description}</span>
           </div>
-          <select
-            className="select select-bordered w-full max-w-xs"
-            value={value}
-            onChange={(e) => setNodeValue(e.target.value)}
-          >
+          <RadioGroup onValueChange={(val) => setNodeValue(val)} required>
             {node.marker.options!.map((option, index) => (
-              <option key={index} value={option}>
-                {option}
-              </option>
+              <div key={index} className="flex items-center space-x-2">
+                <RadioGroupItem value={option} id={id + '-' + index} />
+                <Label htmlFor={id + '-' + index}>{option}</Label>
+              </div>
             ))}
-          </select>
+          </RadioGroup>
         </label>
       );
   }
