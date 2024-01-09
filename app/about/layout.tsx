efbd91d9ff4046b47df54f90335474c9bad9da84
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2024-01-08 20:09:48 -0800

    Add about page

---
+++ app/about/layout.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'About - BBCode Templater',
+};
+
+export default function DocsLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <main>
+      <article className="prose prose-sm prose-slate w-full max-w-4xl flex-grow md:prose-base dark:prose-invert">
+        {children}
+      </article>
+    </main>
+  );
+}
