3af9863c58ef7315206675d0be9b67f9525461ee
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2024-01-09 19:52:54 -0800

    Add dynamic metadata for pastebin input

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,13 +1,13 @@
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Documentation - BBCode Templater',
+  title: 'Documentation | BBCode Templater',
 };
 
 export default function DocsLayout({ children }: { children: React.ReactNode }) {
   return (
     <main>
-      <article className="prose prose-sm md:prose-base dark:prose-invert prose-slate w-full max-w-4xl flex-grow">
+      <article className="prose prose-sm prose-slate w-full max-w-4xl flex-grow md:prose-base dark:prose-invert">
         {children}
       </article>
     </main>
--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,45 @@ import { TemplateProvider } from '@/components/template-provider';
 import { TemplateInput } from '@/components/templater/template-input';
 import { GenerateOutput } from '@/components/templater/generate-output';
 import { TemplateDebug } from '@/components/templater/template-debug';
+import { Metadata } from 'next';
 
 type Props = {
-  searchParams: { pastebin: string };
+  searchParams?: { pastebin?: string };
 };
 
-export default async function Home({ searchParams: { pastebin } }: Props) {
-  const pastebinurl = pastebin ? 'https://pastebin.com/' + pastebin : '';
+export async function generateMetadata({ searchParams }: Props): Promise<Metadata> {
+  const metadata: Metadata = {
+    title: 'Text Templater',
+    description: 'Plain Text Template to Form Field Generator',
+  };
+
+  if (searchParams?.pastebin) {
+    const pastebin = searchParams.pastebin;
+    const pasteBinRes = await getPastebin(`https://pastebin.com/${pastebin}`);
+    if ('error' in pasteBinRes) {
+      return metadata;
+    } else {
+      const pasteBinContent = pasteBinRes.metadata;
+      if (pasteBinContent.title) {
+        metadata.title = pasteBinContent.title + ' - Text Templater';
+        metadata.description = 'Plain Text Template to Form Field Generator for: ' + pasteBinContent.title;
+      }
+    }
+  }
+
+  return metadata;
+}
+
+export default async function Home({ searchParams }: Props) {
+  console.log('rendered home', searchParams);
+  const pastebinurl = searchParams?.pastebin ? 'https://pastebin.com/' + searchParams.pastebin : '';
   const pastebinObj = {
     url: pastebinurl,
     content: {} as getPastebinResult,
     error: '',
   };
-  if (pastebin) {
-    const res = await getPastebin(`https://pastebin.com/${pastebin}`);
+  if (pastebinurl) {
+    const res = await getPastebin(pastebinurl);
     if ('error' in res) {
       pastebinObj.error = res.error;
     } else {
@@ -27,7 +52,7 @@ export default async function Home({ searchParams: { pastebin } }: Props) {
 
   return (
     <main className="flex min-h-screen flex-col items-start gap-4">
-      <TemplateProvider templateInit={pastebinObj.content?.content || ''}>
+      <TemplateProvider key={pastebinObj.content?.content || ''} templateInit={pastebinObj.content?.content || ''}>
         <TemplateInput pastebin={pastebinObj} />
         <label className="form-control relative w-full min-w-full flex-row flex-wrap">
           <TemplateDebug />
